53634a7602f233404d6aae446e4c28c9e635502a
Alexey Taktarov <molefrog@gmail.com>
2023-05-17 11:05:16 +0200

    Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,25 @@ export const Home = () => {
       <br />
       <p>
         This demo project shows how wouter can be used with Ultra.js, a Deno framework. The upcoming
-        2.11.0 release of wouter has a built-in support for server-side rendering via a simple prop
-        for defining server-side location.
+        2.11.0 release of wouter has a built-in support for SSR via a simple prop for defining
+        server-side location.
       </p>
+
+      <p>
+        Features in this demo:
+        <ul>
+          <li>Server-side rendering</li>
+          <li>Navigation links (active state)</li>
+          <li>Switch with default route (404)</li>
+          <li>
+            Optional locale segment and nested routing <code>/en/users</code>
+          </li>
+        </ul>
+      </p>
+
+      <a href="https://github.com/molefrog/wultra">
+        <button>Website's source on GitHub →</button>
+      </a>
     </>
   );
 };
